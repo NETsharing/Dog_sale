bf56b356319d596aae2293abe5f18eb172c77edd
Netsharing <workcenter@mail.ru>
2019-11-28 18:16:26 +0300

    +comments

--- PetShop/settings.py
+++ PetShop/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
