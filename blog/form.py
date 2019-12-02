d37682b99ad1d7d60b9baa59f4f2286f9b378193
Netsharing <workcenter@mail.ru>
2019-12-02 17:44:03 +0300

    Pagination

--- blog/form.py
+++ blog/form.py
@@ -1,7 +1,14 @@
 from django import forms
+from .models import Comment
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(max_length=100, required=False, widget=forms.Textarea)
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')    # явно указываем какие поля хотим включить
+        # exclude = ('updated')                   # поля которые явно исключаем
--- blog/views.py
+++ blog/views.py
@@ -1,33 +1,50 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.views.generic import ListView
-from .form import EmailPostForm
+from .form import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
-# def post_list(request):         # выводим только запосченные объявления
-#     object_list = Post.published.all()
-#     paginator = Paginator(object_list, 3)                       # по 3 поста на каждой странице
-#     page = request.GET.get('page')
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)                               # Если страница не является целым числом, отправляем первую страницу
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)             # Если страница вне диапазона отправляем последнюю страницу результатов
-#     return render(request, 'blog/post/list.html',{'page': page, 'posts': posts})
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
+def post_list(request, tag_slug=None):         # выводим только запосченные объявления
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)                       # по 3 поста на каждой странице
+    page = request.GET.get('page')
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list =object_list.filter(tags__in=[tag])
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)                               # Если страница не является целым числом, отправляем первую страницу
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)             # Если страница вне диапазона отправляем последнюю страницу результатов
+    return render(request, 'blog/post/list.html',{'page': page, 'posts': posts, 'tag':tag})
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
                              status='published',
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    # Список активных комментариев к этой записи
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method =='POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)           # Создать объект комментариев, но еще не сохранять его в базе
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments,
+                                                     'new_comment': new_comment,'comment_form': comment_form})
 def post_share(request, post_id):                                   # Получаем сообщение по его id
     post = get_object_or_404(Post, id=post_id,  status='published')
     sent = False
@@ -43,3 +60,5 @@ def post_share(request, post_id):                                   # Получ
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post' : post, 'form': form, 'sent': sent})
+
+
